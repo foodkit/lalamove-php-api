9dc879f72acf292b69973f84a8ef4c007dd01566
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2018-08-31 10:00:01 +0500

    enforce php version allow set optional Logger add getSettings() to LalamoveRequest class add logging feature to GuzzleTransport class add test that failed request will still be logged add psr logger to composer.lock file

--- src/Http/GuzzleTransport.php
+++ src/Http/GuzzleTransport.php
@@ -2,8 +2,28 @@
 
 namespace Lalamove\Http;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\TransferException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use function GuzzleHttp\Psr7\str;
+
 class GuzzleTransport implements TransportInterface
 {
+    /** @var Client */
+    private $client;
+
+    /**
+     * GuzzleTransport constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client = null)
+    {
+        $this->client = $client ?? new Client();
+    }
+
+
     /**
      * @param LalamoveRequest $request
      * @return mixed
@@ -11,27 +31,91 @@ class GuzzleTransport implements TransportInterface
      */
     public function send(LalamoveRequest $request)
     {
-        $method = $request->getMethod();
-        $uri = $request->getFullPath();
-        $params = $request->getParams();
+        $method  = $request->getMethod();
+        $uri     = $request->getFullPath();
+        $params  = $request->getParams();
         $headers = $request->getHeaders();
 
-        $payload = ['headers' => $headers];
+        $payload                                       = ['headers' => $headers];
         $payload[$method === 'GET' ? 'query' : 'json'] = $params;
 
-        $response = $this->client()->request($method, $uri, $payload);
+        try {
+            $response = $this->client->request($method, $uri, $payload);
+            $this->logRequestResponse($request, $response);
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $this->logRequestResponse($request, $e->getResponse());
+            } else {
+                $this->logRequestFailure($request, $e->getMessage());
+            }
+            throw $e;
+        } catch (TransferException $e) {
+            $this->logRequestFailure($request, $e->getMessage());
+            throw $e;
+        }
 
-        $body = $response->getBody()->getContents();
+        $body   = $response->getBody()->getContents();
         $result = json_decode($body);
 
         return $result;
     }
 
+
     /**
-     * @return \GuzzleHttp\Client
+     * Log request/response details
+     * @see https://github.com/guzzle/psr7#function-str
+     *
+     * @param LalamoveRequest $request
+     * @param Response $response
+     * @return void
      */
-    protected function client()
+    protected function logRequestResponse(LalamoveRequest $request, Response $response)
     {
-        return new \GuzzleHttp\Client();
+        if (!$request->getSettings()->logger) {
+            return;
+        }
+
+        $message = str($this->convertRequest($request));
+        $message .= PHP_EOL;
+        $message .= str($response);
+
+        $request->getSettings()->logger->info($message);
+    }
+
+    /**
+     * Log request which failed before connecting to the server
+     * @param LalamoveRequest $request
+     * @param string $message
+     * @return void
+     */
+    protected function logRequestFailure(LalamoveRequest $request, string $failureMessage)
+    {
+        if (!$request->getSettings()->logger) {
+            return;
+        }
+
+        $message = str($this->convertRequest($request));
+        $message .= PHP_EOL;
+        $message .= $failureMessage;
+
+        $request->getSettings()->logger->info($message);
+    }
+
+    /**
+     * Helper method to convert a domain LalamoveRequest to the guzzle's one
+     *
+     * @param LalamoveRequest $request
+     * @return Request
+     */
+    protected function convertRequest(LalamoveRequest $request): Request
+    {
+        $uri  = $request->getFullPath();
+        $body = "";
+        if ($request->getMethod() == "GET") {
+            $uri .= "?" . http_build_query($request->getParams());
+        } else {
+            $body = json_encode($request->getParams());
+        }
+        return new Request($request->getMethod(), $uri, $request->getHeaders(), $body);
     }
 }
