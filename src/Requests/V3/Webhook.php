c8e515f2625bd1b4dd8d2dd9224797a96b1b6446
Anjo Tadena <tadena.anjo@gmail.com>
2022-04-27 12:09:37 +0800

    [sc-11209] - Handle coordinates undefined value

--- src/Requests/V3/Webhook.php
+++ src/Requests/V3/Webhook.php
@@ -6,6 +6,18 @@ namespace Lalamove\Requests\V3;
 
 class Webhook
 {
+    public const ASSIGNING_DRIVER = 'ASSIGNING_DRIVER';
+    
+    public const ON_GOING = 'ON_GOING';
+    
+    public const PICKED_UP = 'PICKED_UP';
+    
+    public const COMPLETED = 'COMPLETED';
+    
+    public const CANCELLED = 'CANCELED';
+
+    public const REJECTED = 'REJECTED';
+
     public string $url;
 
     public function __construct(string $url)
