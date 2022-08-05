b0f1110fa684a96dd94135fa1bf313e057659f76
Corey Mcmahon <contact@coreymcmahon.com>
2022-08-05 14:24:18 +0700

    [sc-0] add failing test for https://github.com/foodkit/lalamove-php-api/issues/12

--- tests/Unit/Resources/V3QuotationsResourceTest.php
+++ tests/Unit/Resources/V3QuotationsResourceTest.php
@@ -9,6 +9,7 @@ use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Lalamove\Exceptions\PaymentRequiredException;
 use Lalamove\Requests\V3\Quotation;
+use Lalamove\Responses\V3\QuotationResponse;
 use LalamoveTests\BaseTest;
 use LalamoveTests\Helpers\DummySettings;
 
@@ -68,5 +69,54 @@ class V3QuotationsResourceTest extends BaseTest
         $this->assertObjectHasAttribute('isRouteOptimized', $response);
         $this->assertObjectHasAttribute('priceBreakdown', $response);
         $this->assertObjectHasAttribute('item', $response);
+    }
+
+    public function test_it_maps_response()
+    {
+        // Sample payload from: https://developers.lalamove.com/#get-quotation
+        $sampleResponse = "{
+    \"data\": {
+        \"quotationId\": \"1514140994227007571\",
+        \"scheduleAt\": \"2022-04-13T07:18:38.00Z\",
+        \"expiresAt\": \"2022-04-13T07:23:39.00Z\",
+        \"serviceType\": \"MOTORCYCLE\",
+        \"specialRequests\": [
+            \"TOLL_FEE_10\",
+            \"PURCHASE_SERVICE_1\"
+        ],
+        \"language\": \"EN_HK\",
+        \"stops\": [
+            {
+                \"stopId\": \"1514140995971838016\",
+                \"coordinates\": {
+                    \"lat\": \"22.3354735\",
+                    \"lng\": \"114.1761581\"
+                },
+                \"address\": \"Innocentre, 72 Tat Chee Ave, Kowloon Tong\"
+            },
+            {
+                \"stopId\": \"1514140995971838017\",
+                \"coordinates\": {
+                    \"lat\": \"22.2812946\",
+                    \"lng\": \"114.1598610\"
+                },
+                \"address\": \"Statue Square, Des Voeux Rd Central, Central\"
+            }
+        ],
+        \"isRouteOptimized\": false,
+        \"priceBreakdown\": {
+            \"base\": \"90\",
+            \"specialRequests\": \"13\",
+            \"vat\": \"21\",
+            \"totalBeforeOptimization\": \"124\",
+            \"totalExcludePriorityFee\": \"124\",
+            \"total\": \"124\",
+            \"currency\": \"HKD\"
+        }
+    }
+}";
+        $json = json_decode($sampleResponse);
+        $quotation = new QuotationResponse($json);
+        self::assertInstanceOf(QuotationResponse::class, $quotation);
     }
 }
